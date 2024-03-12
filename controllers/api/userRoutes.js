e4d5948e12b63d835747f64277359f592780c8da
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-12 01:28:34 -0500

    finished the route files

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -23,10 +23,40 @@ router.post('/signup', async (req, res) => {
 
 // POST route for user login
 router.post('/login', async (req, res) => {
-});
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+      if (!userData) {
+        res.status(400).json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await bcrypt.compare(req.body.password, userData.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.userId = userData.id;
+        req.session.logged_in = true;
+  
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 // POST route for user logout
 router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    // Destroy the session
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
